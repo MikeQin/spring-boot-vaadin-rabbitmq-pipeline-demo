d364ba6559d9ac96c254dcb202cd788843a818fb
Michael Qin <mike.qin@gmail.com>
2017-04-07 14:30:57 -0400

    Update EventTest.java

--- src/test/java/app/EventTest.java
+++ src/test/java/app/EventTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +17,17 @@ import app.Event;
 import app.EventRepository;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {AppConfig.class, App.class})
+@SpringBootTest(classes = { AppConfig.class, App.class })
 public class EventTest {
 
 	@Autowired
 	EventRepository repository;
+
+	@Before
+	public void setup() {
+		repository.deleteAllInBatch();
+		repository.flush();
+	}
 
 	@Test
 	public void contextLoads() {
@@ -33,40 +40,40 @@ public class EventTest {
 
 		repository.save(expected);
 		Event actual = repository.findAll().get(0);
-		
+
 		assertThat(actual).isEqualTo(expected);
 	}
-	
+
 	@Test
 	public void valueOf() {
-		
+
 		Event expected = new Event();
 		String eventStr = expected.toString();
 		Event actual = Event.valueOf(eventStr);
-		
+
 		assertThat(actual).isEqualTo(expected);
-		
+
 		System.out.println("actual: " + actual);
 	}
-	
+
 	@Test
 	public void findByType() {
 		List<Event> events = new ArrayList<>();
-		
-		for(int i=0; i<10; i++) {
+
+		for (int i = 0; i < 10; i++) {
 			events.add(new Event("INFO"));
 		}
-		
-		for(int i=0; i<5; i++) {
+
+		for (int i = 0; i < 5; i++) {
 			events.add(new Event("ERROR"));
 		}
-		
+
 		repository.save(events);
 		repository.flush();
-		
+
 		List<Event> infos = repository.findByType("INFO");
 		List<Event> errors = repository.findByType("ERROR");
-		
+
 		assertThat(10).isEqualTo(infos.size());
 		assertThat(5).isEqualTo(errors.size());
 	}
